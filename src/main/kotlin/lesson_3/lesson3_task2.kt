993f1bf3ca8a4cbc3fb143a2219bf5f4622a8297
Vlada Vasileva <vvavlada@gmail.com>
2025-03-19 23:45:04 +0300

    KS-3-2 сделала так, что изменяются старые переменные

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+    var firstName: String
+    firstName = "Татьяна"
+    var lastName: String
+    lastName = "Андреева"
+    var middleName: String
+    middleName = "Сергеевна"
+    var age: Int
+    age = 20
+
+    println("ФИО: $lastName $firstName $middleName, возраст: $age")
+
+    lastName  = "Сидорова"
+    age = 22
+
+    println("ФИО: $lastName $firstName $middleName, возраст: $age")
