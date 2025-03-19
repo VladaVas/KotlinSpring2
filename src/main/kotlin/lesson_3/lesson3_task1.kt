f3403466af65b023eae66cabc4b41dd05026ff77
Vlada Vasileva <vvavlada@gmail.com>
2025-03-19 22:50:24 +0300

    KS-3-1 вынесла приветсвия в отдельные переменные

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "Salomon"
+
+    var greeting: String
+
+    greeting  = "Good morning"
+    println("$greeting, $userName")
+
+    greeting = "Good evening"
+    println("$greeting, $userName")
+}
