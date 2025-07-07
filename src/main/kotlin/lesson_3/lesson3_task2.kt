0d63c66d354087402ca525ed19dc8d1ab0107616
Vlada Vasileva <vvavlada@gmail.com>
2025-07-07 23:56:29 +0300

    KS-3-2 Исправила замечение: переписала данные в старые переменные + создала новую ветку

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,18 +1,16 @@
 package org.example.lesson_3
 
 fun main() {
-    var firstName: String
-    firstName = "Татьяна"
-    var lastName: String
-    lastName = "Андреева"
-    var middleName: String
-    middleName = "Сергеевна"
-    var age: Int
-    age = 20
+    var firstName = "Татьяна"
+    var lastName = "Андреева"
+    var middleName = "Сергеевна"
+    var age = 20
 
     println("ФИО: $lastName $firstName $middleName, возраст: $age")
 
-    lastName  = "Сидорова"
+    lastName = "Сидорова"
     age = 22
 
     println("ФИО: $lastName $firstName $middleName, возраст: $age")
+
+}
