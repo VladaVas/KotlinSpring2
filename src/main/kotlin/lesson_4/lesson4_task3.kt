44b2dd54eaa743fd49dd9e7eb824fa15b7d1386d
Vlada Vasileva <vvavlada@gmail.com>
2025-07-21 22:20:41 +0300

    KS-4-3 вынесла критерии в константу

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_4
 
+const val SUNNY_WEATHER = true
+const val IS_TENT_OPEN = true
+const val HUMIDITY = 20
+const val CURRENT_SEASON = "зима"
+
 fun main() {
 
-    val sunnyWeather = true
-    val isTentOpen = true
-    val humidity = 20
-    val currentSeason = "зима"
-
-    val isFavorable = sunnyWeather &&
-            isTentOpen &&
-            humidity == 20 &&
-            currentSeason != "зима"
+    val isFavorable = SUNNY_WEATHER &&
+            IS_TENT_OPEN &&
+            HUMIDITY == 20 &&
+            CURRENT_SEASON != "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorable")
 }
