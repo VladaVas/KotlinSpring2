c87ab44312ed991ba5ca0f97e23f3c827a692b4a
Vlada Vasileva <vvavlada@gmail.com>
2025-07-24 00:50:56 +0300

    KS-4-5 добавила явное сравнение через == для логических значений

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_4
 
 const val IS_DAMAGED = false
-val CREW_COUNT = 60
-val PROVISIONS = 55
-val IS_WEATHER_FAVORABLE = true
+const val CREW_COUNT = 60
+const val PROVISIONS = 55
+const val IS_WEATHER_FAVORABLE = true
 
 fun main() {
 
-    val canDepart = (!IS_DAMAGED &&
+    val canDepart = (!IS_DAMAGED == true &&
             CREW_COUNT in 55..70 &&
             PROVISIONS > 50) ||
             (IS_DAMAGED &&
                     CREW_COUNT == 70 &&
-                    IS_WEATHER_FAVORABLE &&
+                    IS_WEATHER_FAVORABLE == true &&
                     PROVISIONS >= 50)
 
     println("Корабль может отправиться в плавание: $canDepart")
