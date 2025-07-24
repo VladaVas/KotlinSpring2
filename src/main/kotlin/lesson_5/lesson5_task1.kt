1fe77b487b4c09153cd49ea989c4455c1cb9f2bb
Vlada Vasileva <vvavlada@gmail.com>
2025-07-24 03:21:29 +0300

    KS-5-1 вынесла числа из примера в переменные, чтобы было проще обслуживать код

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -3,7 +3,10 @@ package org.example.lesson_5
 const val RIGHT_ANSWER = 4
 fun main() {
 
-    println("Напишите резение примера: 2 + 2 = ")
+    val number1 = 2
+    val number2 = 2
+
+    println("Напишите решение примера: $number1 + $number2 = ")
     val userAnswer = readln().toInt()
 
     if (userAnswer == RIGHT_ANSWER) {
