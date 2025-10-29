511b38af9325d9905d4466f8871b3d650130abbd
Vlada Vasileva <vvavlada@gmail.com>
2025-10-29 18:18:17 +0300

    KS-5-1 сделала так, чтобы сумма считалась из переменных

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_5
 
-const val RIGHT_ANSWER = 4
 fun main() {
 
     val number1 = 2
     val number2 = 2
+    val rightAnswer = number1 + number2
 
     println("Напишите решение примера: $number1 + $number2 = ")
     val userAnswer = readln().toInt()
 
-    if (userAnswer == RIGHT_ANSWER) {
+    if (userAnswer == rightAnswer) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
