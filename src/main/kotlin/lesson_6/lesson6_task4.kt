d414c2c7981fe7f4901322150fd6733e641480aa
Vlada Vasileva <vvavlada@gmail.com>
2025-11-05 23:31:02 +0300

    KS-6-4 решение задачи

---
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_6
+
+fun main () {
+
+    val randomNumber = (1..9).random()
+    var attempts = 5
+
+    println("Угадай число от 1 до 9! У тебя $attempts попыток.")
+
+    while (attempts > 0) {
+        println("Введи число: ")
+        val userNumber = readln().toInt()
+
+        if (userNumber == randomNumber) {
+            println("Это была великолепная игра! Ты угадал число $randomNumber!")
+            break
+        } else {
+            attempts--
+            if (attempts > 0 && userNumber != randomNumber){
+                println("Неверно. Попробуй еще раз. У тебя осталось $attempts попыток")
+            } else {
+                println("Попытки закончились! Было загадано число: $randomNumber.")
+            }
+        }
+    }
+}
