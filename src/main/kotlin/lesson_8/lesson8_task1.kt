288ab57276b7f642e7d6c39d5f57f882106e0c7f
Vlada Vasileva <vvavlada@gmail.com>
2025-11-18 18:40:42 +0300

    KS-8-1 исправила замечания

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -3,10 +3,7 @@ package org.example.lesson_8
 fun main() {
 
     val weeklyViews = arrayOf(325, 514, 216, 370, 181, 31, 278)
-    var totalViews = 0
+    val totalViews = weeklyViews.sum()
 
-    for(views in weeklyViews) {
-        totalViews += views
-    }
     println("За неделю было $totalViews просмотров")
 }
