932bfc5e90e83a5ce6e506007d7bf7661ccb8a2e
Vlada Vasileva <vvavlada@gmail.com>
2025-11-18 18:34:46 +0300

    KS-8-5 исправила замечания

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -2,21 +2,20 @@ package org.example.lesson_8
 
 fun main() {
 
-    val listOfIngridients = arrayListOf("творог", "рисовая мука", "яйцо", "ванилин", "сахар")
-    println("Список ингридиетов: $listOfIngridients")
+    val listOfIngridients = arrayOf("творог", "рисовая мука", "яйцо", "ванилин", "сахар")
+    println("Список ингридиетов: ${listOfIngridients.joinToString(", ")}")
 
     println("Введите ингридет, который вы хотите заменить: ")
     val ingridientSearch = readln()
+    val index = listOfIngridients.indexOf(ingridientSearch)
 
-    if (ingridientSearch in listOfIngridients) {
+    if (index >= 0) {
         println("Введите ингридиет, который вы хотели бы добавить: ")
         val ingridientFromUser = readln()
 
-        val index = listOfIngridients.indexOf(ingridientSearch)
         listOfIngridients[index] = ingridientFromUser
-        println("Готово! Вы сохранили следующий список: $listOfIngridients")
+        println("Готово! Вы сохранили следующий список: ${listOfIngridients.joinToString(", ")}")
     } else {
         println("Такого ингредиента в рецепте нет.")
-
     }
 }
