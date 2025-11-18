5ceffcba9cc4d0c3fd66d9cf5a098ea723bd58aa
Vlada Vasileva <vvavlada@gmail.com>
2025-11-18 18:41:57 +0300

    KS-8-2 исправила замечания

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -2,13 +2,13 @@ package org.example.lesson_8
 
 fun main() {
 
-    val listOfIngridients = arrayListOf("творог", "рисовая мука", "яйцо", "ванилин", "сахар")
+    val listOfIngridients = arrayOf("творог", "рисовая мука", "яйцо", "ванилин", "сахар")
 
     println("Введите ингридет для поиска: ")
     val ingridientSearch = readln()
 
     for (ingridient in listOfIngridients) {
-        if(ingridient == ingridientSearch) {
+        if (ingridient == ingridientSearch) {
             println("Ингредиент $ingridientSearch в рецепте есть.")
             return
         }
