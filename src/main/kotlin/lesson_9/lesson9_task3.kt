0da512a7fd9c44425617755ca5df5382df3db045
Vlada Vasileva <vvavlada@gmail.com>
2025-11-22 00:39:35 +0300

    KS-9-3 переделала решение

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -4,25 +4,11 @@ fun main() {
 
     val omletIngridients = listOf(2, 50, 15)
 
-     println("Введите количество проций: ")
+    println("Введите количество проций: ")
     val countOfPortions = readln().toInt()
 
-    val eggs = omletIngridients.filter {
-        it == omletIngridients[0]
-    }
-    val totalEggs = eggs.map { it * countOfPortions }
+    val totalPortions = omletIngridients.map { it * countOfPortions }
 
-    val milk = omletIngridients.filter {
-        it == omletIngridients[1]
-    }
-    val totalMilk = milk.map { it * countOfPortions}
-
-    val butter = omletIngridients.filter {
-        it == omletIngridients[2]
-    }
-    val totalButter = butter.map { it * countOfPortions}
-
-    println("На $countOfPortions порций вам понадобится: Яиц – $totalEggs, молока – $totalMilk, сливочного масла – $totalButter")
-
+    println("На $countOfPortions порций вам понадобится: Яиц – ${totalPortions[0]}, молока – ${totalPortions[1]}, сливочного масла – ${totalPortions[2]}.")
 
 }
