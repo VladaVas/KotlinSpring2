8909d99abff1e0cd6cc65f6186cf5ad393a9ce58
Vlada Vasileva <vvavlada@gmail.com>
2025-11-25 23:30:06 +0300

    KS-10-4 решение задачи

---
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -0,0 +1,54 @@
+package org.example.lesson_10
+
+fun main() {
+
+    var playerWins = 0
+
+    do {
+        val result = playRound()
+
+        when (result) {
+            1 -> {
+                println("Победило человечество")
+                playerWins++
+            }
+
+            2 -> {
+                println("Победила машина")
+            }
+
+            3 -> {
+                println("Победила дружба")
+            }
+        }
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        val playerAnswer = readln()
+
+    } while (playerAgreed(playerAnswer))
+
+    println("Человек выиграл $playerWins партию(й)")
+}
+
+fun rollDice(): Int {
+    return (1..6).random()
+}
+
+fun playRound(): Int {
+    println("Игрок делает ход.")
+    val playerRoll = rollDice()
+    println("Игрок выбросил $playerRoll")
+
+    println("Компьютер делает ход.")
+    val compucterRoll = rollDice()
+    println("Компьютер выбросил $compucterRoll")
+
+    return when {
+        playerRoll > compucterRoll -> 1
+        playerRoll < compucterRoll -> 2
+        else -> 3
+    }
+}
+
+fun playerAgreed(yesAnswer: String): Boolean {
+    return yesAnswer.equals("Да", ignoreCase = true)
+}
