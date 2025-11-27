73bf74824bb25abb4e8b17f9cbeed6abafb693e8
Vlada Vasileva <vvavlada@gmail.com>
2025-11-27 21:51:51 +0300

    KS-11-4 - решение задачи

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_11
+
+class RecipeCategory(
+    val id: String,
+    val name: String,
+    val image: String,
+) {
+    val recipeList = mutableListOf<Recipe>()
+}
+
+class Recipe(
+    val id: String,
+    val image: String,
+    val title: String,
+    val categoryID: String,
+    val portions: String,
+    val ingridients: String,
+    val steps: String,
+) {}
+
+class Ingredient(
+    val name: String,
+    val amount: Double,
+    val unit: String,
+)
