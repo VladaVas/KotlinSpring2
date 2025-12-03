e99fcde7842f1981541ec87500519df9a7e41ecc
Vlada Vasileva <vvavlada@gmail.com>
2025-12-04 01:27:30 +0300

    KS-11-4 - добавила конструктор (IDEA сама такое решение предложила)

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,22 +1,46 @@
 package org.example.lesson_11
 
-class RecipeCategory(
-    val id: String,
-    val name: String,
-    val image: String,
-) {
-    val recipeList = mutableListOf<Recipe>()
+class RecipeCategory {
+    val id: String
+    val name: String
+    val image: String
+    val recipeList: MutableList<Recipe>
+
+    constructor(id: String, name: String, image: String, recipeList: MutableList<Recipe>) {
+        this.id = id
+        this.name = name
+        this.image = image
+        this.recipeList = recipeList
+    }
 }
 
-class Recipe(
-    val id: String,
-    val image: String,
-    val title: String,
-    val categoryID: String,
-    val portions: String,
-    val ingridients: String,
-    val steps: String,
-)
+class Recipe {
+    val id: String
+    val image: String
+    val title: String
+    val categoryID: String
+    val portions: String
+    val ingridients: List<Ingredient>
+    val steps: String
+
+    constructor(
+        id: String,
+        image: String,
+        title: String,
+        categoryID: String,
+        portions: String,
+        ingridients: List<Ingredient>,
+        steps: String
+    ) {
+        this.id = id
+        this.image = image
+        this.title = title
+        this.categoryID = categoryID
+        this.portions = portions
+        this.ingridients = ingridients
+        this.steps = steps
+    }
+}
 
 class Ingredient(
     val name: String,
