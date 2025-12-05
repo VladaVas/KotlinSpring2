53251a97bc8624f84ca0b885d6ce24ca751ed21c
Vlada Vasileva <vvavlada@gmail.com>
2025-12-05 21:34:15 +0300

    KS-11-4 - переместила свойства в конструктор класса

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,49 +1,24 @@
 package org.example.lesson_11
 
-class RecipeCategory {
-    val id: String
-    val name: String
-    val image: String
-    val recipeList: MutableList<Recipe>
+class RecipeCategory(
+    val id: String,
+    val name: String,
+    val image: String,
+    val recipeList: MutableList<Recipe>,
+) {}
 
-    constructor(id: String, name: String, image: String, recipeList: MutableList<Recipe>) {
-        this.id = id
-        this.name = name
-        this.image = image
-        this.recipeList = recipeList
-    }
-}
-
-class Recipe {
-    val id: String
-    val image: String
-    val title: String
-    val categoryID: String
-    val portions: String
-    val ingridients: List<Ingredient>
-    val steps: String
-
-    constructor(
-        id: String,
-        image: String,
-        title: String,
-        categoryID: String,
-        portions: String,
-        ingridients: List<Ingredient>,
-        steps: String
-    ) {
-        this.id = id
-        this.image = image
-        this.title = title
-        this.categoryID = categoryID
-        this.portions = portions
-        this.ingridients = ingridients
-        this.steps = steps
-    }
-}
+class Recipe(
+    val id: String,
+    val image: String,
+    val title: String,
+    val categoryID: String,
+    val portions: String,
+    val ingridients: List<Ingredient>,
+    val steps: String,
+) {}
 
 class Ingredient(
     val name: String,
     val amount: Double,
     val unit: String,
-)
+){}
