e9cbcc25d1b62a309cce4172f03da8cc427b8649
Vlada Vasileva <vvavlada@gmail.com>
2025-12-09 19:03:19 +0300

    KS-13-1 - решение задачи

---
+++ src/main/kotlin/lesson_13/lessom13_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_13
+
+class Phonebook(_name: String, _phoneNumber: Long, _company: String?) {
+
+    val name: String = _name
+    val phoneNumber: Long = _phoneNumber
+    val company: String? = _company
+
+}
