376f8de24b5f2517de7a7047c9dcb62386c85069
Vlada Vasileva <vvavlada@gmail.com>
2025-12-18 01:59:24 +0300

    KS-14-1 - добавила переопределение через override

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,57 +1,37 @@
 package org.example.lesson_14
 
-open class Vessel(
-    val name: String,
+abstract class Liner(
+    open val name: String,
     open val speed: Int = 250,
     open val capacity: Int = 400,
     open val liftingCapacity: Int = 300,
-   open val icebreakingAbility: Boolean = false,
-) {}
-
-class Liner(
-    name: String,
-    speed: Int = 250,
-    capacity: Int = 500,
-    liftingCapacity: Int = 200,
-) : Vessel(
-    name = name,
-    speed = speed,
-    capacity = capacity,
-    liftingCapacity = liftingCapacity,
+    open val icebreakingAbility: Boolean = false,
 )
+
 class Cargoship(
-    name: String,
-    speed: Int = 150,
-    capacity: Int = 300,
-    liftingCapacity: Int = 400,
-): Vessel(
+    override val name: String,
+) : Liner(
     name = name,
-    speed = speed,
-    capacity = capacity,
-    liftingCapacity = liftingCapacity,
-) {}
+    speed = 250,
+    capacity = 500,
+    liftingCapacity = 200
+)
 
 class Icebreaker(
-    name: String,
-    speed: Int = 200,
-    capacity: Int = 250,
-    liftingCapacity: Int = 300,
-    icebreakingAbility: Boolean = true,
-): Vessel(
+    override val name: String,
+) : Liner(
     name = name,
-    speed = speed,
-    capacity = capacity,
-    liftingCapacity = liftingCapacity,
-    icebreakingAbility = icebreakingAbility,
-) {}
+    speed = 200,
+    capacity = 250,
+    liftingCapacity = 300,
+    icebreakingAbility = true,
+    )
 
 fun main() {
 
-    val liner = Liner("Лайнер \"Анатолий\"",)
     val cargoship = Cargoship("Грузовое судно \"Слон Вася\"")
     val icebreaker = Icebreaker("Ледокол \"Мишка на севере\"")
 
-    println("Имя: ${liner.name}, Скорость: ${liner.speed} км/ч")
     println("Имя: ${cargoship.name}, Скорость: ${cargoship.speed} км/ч")
     println("Имя: ${icebreaker.name}, Скорость: ${icebreaker.speed} км/ч")
 }
