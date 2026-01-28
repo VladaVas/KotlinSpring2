e56281bb0bba7970a831d181ffe9cfdc6219fce5
Vlada Vasileva <vvavlada@gmail.com>
2026-01-29 02:01:52 +0300

    KS-19-3 - решение задачи

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_19
+
+class SpaceShip {
+
+    fun takingOf() {
+        TODO("Need additional logic information")
+    }
+    fun landing() {
+        TODO()
+    }
+    fun shooting() {}
+}
+
+fun main() {
+
+    val spaceShip = SpaceShip()
+
+    spaceShip.landing()
+}
