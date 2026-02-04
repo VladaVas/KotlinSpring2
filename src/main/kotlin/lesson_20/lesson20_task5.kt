8a6913af8dc5a9113bf3e70015d0a0bd78cc47c6
Vlada Vasileva <vvavlada@gmail.com>
2026-02-04 20:13:38 +0300

    KS-20-5 - решение задачи

---
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -0,0 +1,39 @@
+package org.example.lesson_20
+
+class Robot(
+    private val phrases: List<String> = listOf(
+        "Привет человек",
+        "Я люблю программирование",
+        "Kotlin это весело",
+        "Роботы захватят мир",
+        "Хорошего дня"
+    )
+) {
+
+    private var modifier: ((String) -> String)? = null
+
+    fun say() {
+        val phrase = phrases.random()
+        val result = modifier?.invoke(phrase) ?: phrase
+        println(result)
+    }
+
+    fun setModifier(modifier: (String) -> String) {
+        this.modifier = modifier
+        }
+    }
+
+
+fun main() {
+
+    val robot = Robot()
+    robot.say()
+
+    robot.setModifier { text ->
+        text.split(" ")
+            .reversed()
+            .joinToString(" ")
+    }
+
+    robot.say()
+}
